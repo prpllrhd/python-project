bf67ae3f7c6ab063b98a94b15bd7a05260e88ee4
sameer gawande <prpllrhd@yahoo.com>
2019-03-17 20:53:12 -0400

    completed exampleInheritance.py

--- learn.class/exampleInheritance.py
+++ learn.class/exampleInheritance.py
@@ -19,6 +19,29 @@ class Developer(Employee):
 	def __init__(self,first,last,salary,prog_lang):
 		super().__init__(first,last,salary)
 		self.prog_lang = prog_lang
+
+class Manager(Employee):
+	raise_amount = 1.20
+	def __init__(self,first,last,salary,employees=None):
+		super().__init__(first,last,salary)
+		if employees is None:
+			self.employees = []
+		else:
+			self.employees = employees
+
+	def add_employee(self,emp):
+		if emp not in self.employees:
+			self.employees.append(emp)
+	
+	def remove_employee(self,emp):
+		if emp in self.employees:
+			self.employees.remove(emp)
+
+	def print_emp(self):
+		for emp in self.employees:
+			print("---->", emp.print_fullname())
+
+
 	
 
 emp1 = Employee("abhijeet","jadhav",25000)
@@ -26,7 +49,14 @@ print(emp1.salary)
 emp1.apply_raise()
 print(emp1.salary)
 dev1 = Developer("sameer","gawande",500000, "python")
+
 print(dev1.salary)
 dev1.apply_raise()
 print(dev1.salary)
 	
+man1 = Manager("ravi","shastri",75000,[dev1])
+man1.add_employee(emp1)
+man1.add_employee(dev1)
+man1.remove_employee(emp1)
+man1.print_emp()
+print(man1.salary)
